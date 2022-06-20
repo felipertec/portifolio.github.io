9c2ab91930052b68a580293bfec15720cd0607eb
felipertec <felipe.rtec@gmail.com>
2022-06-20 14:36:46 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+let texto1 = document.getElementById('apresentacao1');
+let texto2 = document.getElementById('apresentacao2');
+
+function typeWriter(elemento){
+    const textArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textArray.forEach((letra,i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+    
+}
+
+typeWriter(texto1);
+setTimeout(() =>{
+    typeWriter(texto2);
+    texto2.style.visibility = 'visible';
+}, 4000)
